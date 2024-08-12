faf6a511be36188f32cde4824de51cec58ac6b84
mubarak7785 <mubarakjinabade07@gmail.com>
2024-08-12 20:09:15 +0530

    initial commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Routes,Route } from "react-router-dom";
+
+import { Signup } from "./Components/Signup/Signup";
+import { Home } from "./Components/Home/Home";
+import { Login } from "./Components/Login/Login";
+import { Container } from "@mui/material"; // Import Container for layout
+import { Contact } from "./Components/Contact/Contact";
+import { About } from "./Components/About/About";
+function App() {
+  return (
+    <>
+      <Navbar />
+    
+      <Container sx={{ paddingTop: '64px' }}> {/* Adjust padding based on Navbar height */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </Container>
+    
+     
+    </>
+  );
+}
+
+export default App;
---
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  Button,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import DrawerComp from "./DrawerComp";
+
+const Navbar = () => {
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+
+  return (
+    <React.Fragment>
+      <AppBar sx={{ background: "#063970" }}>
+        <Toolbar>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              height: "100%",
+              maxWidth: "200px",
+            }}
+          >
+            <img
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/max_632/818606114124305.60354e9639250.jpg"
+              alt="Logo"
+              style={{
+                width: "80%",
+                height: "auto",
+                maxHeight: "50px",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
+
+          {isMatch ? (
+            <>
+              <Typography sx={{ fontSize: "2rem", paddingLeft: "10%" }}></Typography>
+              <DrawerComp />
+            </>
+          ) : (
+            <>
+              <Tabs
+                sx={{ marginLeft: "auto" }}
+                indicatorColor="secondary"
+                textColor="inherit"
+                value={value}
+                onChange={(e, newValue) => setValue(newValue)}
+              >
+                <Tab
+                  label="AWS"
+                  component={Link}
+                  to="/aws" // Change to the actual path for AWS page
+                  sx={{ textDecoration: "none", color: "inherit" }}
+                />
+                <Tab
+                  label="DEVOPS"
+                  component={Link}
+                  to="/devops" // Change to the actual path for DevOps page
+                  sx={{ textDecoration: "none", color: "inherit" }}
+                />
+                <Tab
+                  label="ABOUT US"
+                  component={Link}
+                  to="/about" // Change to the actual path for About Us page
+                  sx={{ textDecoration: "none", color: "inherit" }}
+                />
+                <Tab
+                  label="CONTACT"
+                  component={Link}
+                  to="/contact" // Change to the actual path for Contact page
+                  sx={{ textDecoration: "none", color: "inherit" }}
+                />
+              </Tabs>
+              <Button
+                sx={{ marginLeft: "auto" }}
+                variant="contained"
+                component={Link}
+                to="/login"
+              >
+                Login
+              </Button>
+              <Button
+                sx={{ marginLeft: "10px" }}
+                variant="contained"
+                component={Link}
+                to="/signup"
+              >
+                SignUp
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
